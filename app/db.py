50d9760881f31252fcea1658d2fce9ab31091640
Henry Bach <henrypbach@gmail.com>
2023-06-19 10:20:31 -0400

    random ids

--- app/db.py
+++ app/db.py
@@ -1,5 +1,6 @@
 import sqlite3
 import csv
+import random as rng
 
 db_file_name = "database.db"
 
@@ -14,9 +15,16 @@ c.close()
 
 def add_new_painting(painting_name):
     c = db.cursor()
-    c.execute("insert into paintings values(?, ?)", (1, painting_name,))
+    try :
+        id = rng.random()
+        c.execute("insert into paintings values(?, ?)", (id, painting_name,))
+        print("Log: new painting named "+painting_name+" added with id="+str(id))
+    except sqlite3.IntegrityError :
+        add_new_painting(painting_name)
     db.commit()
     c.close()
-    print("Log: new painting named "+painting_name+" added")
+
+def get_paintings():
+    return "a"
 
 #     create TABLE if NOT EXISTS user(u_id int primary key, username varchar(20), password varchar(30));
