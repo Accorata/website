5906f1acef114257c0e33122c459561fa0b8f84b
Henry Bach <henrypbach@gmail.com>
2023-06-29 13:32:50 -0400

    basic add_to_list()

---
+++ app/static/js/list.js
@@ -0,0 +1,21 @@
+var list = document.getElementById('list');
+
+function add_to_list(name) {
+  let new_elem = document.createElement('tr');
+  let new_elem_descrip = document.createElement('th');
+  new_elem_descrip.setAttribute("scope", "row");
+  let new_elem_descrip_text = document.createTextNode("1");
+  new_elem_descrip.appendChild(new_elem_descrip_text);
+  new_elem.appendChild(new_elem_descrip);
+
+  let new_elem_list_elem = document.createElement('td');
+  let new_elem_list_elem_text = document.createTextNode(name);
+  new_elem_list_elem.appendChild(new_elem_list_elem_text);
+
+  new_elem.appendChild(new_elem_list_elem);
+
+  list.appendChild(new_elem);
+  return true;
+}
+
+add_to_list("a");
